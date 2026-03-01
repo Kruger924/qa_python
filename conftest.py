bfe5f969405b65c7fedab9f81c4cd89b42d57963
Sergey Kulikov <Kruger924@yandex.ru>
2026-03-01 18:15:50 +0300

    added fixture in file conftest.py

--- conftest.py
+++ conftest.py
@@ -12,4 +12,12 @@ def pytest_make_parametrize_id(val):
 @pytest.fixture
 def collection_five_books(collection):
     collect = collection
-    books = ['']
+    books = ['Дракула', 'Властелин колец', 'Незнайка на Луне', 'Агата Кристи', '12 стульев']
+    genre = ['Ужасы', 'Фантастика', 'Мультфильмы', 'Детективы', 'Комедии']
+    for i in range(5):
+        collect.add_new_book(books[i])
+
+    for i in range(5):
+        collect.set_book_genre(books[i], genre[i])
+
+    return collect
